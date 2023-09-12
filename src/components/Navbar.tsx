0c10dee1361e5e96d735574865c6f8cde21598d7
alok8bb <alok8bb@gmail.com>
2023-09-12 11:02:47 +0530

    feat: create navbar and heading
    Signed-off-by: alok8bb <alok8bb@gmail.com>

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { FaUserAlt } from 'react-icons/fa'
+import Image from 'next/image'
+
+interface linkProps {
+    to: string,
+    title: string
+    children?: React.ReactNode
+}
+
+const LinkElement: React.FC<linkProps> = ({ to, title, children }) => {
+    return (
+        <div className="hover:bg-blue-600 hover:text-white  p-2 px-6 rounded-3xl">
+            <li className='flex flex-row items-center gap-2'>
+                {children}
+                <a href={to}>{title}</a>
+            </li>
+        </div>
+    )
+}
+
+export const Navbar = () => {
+    return <nav className="flex flex-row py-5 px-10 justify-between">
+        <div>
+            <Image alt='logo' src="/logo.png" width={100} height={100} />
+        </div>
+        <div>
+            <ul className="flex flex-row gap-10">
+                <LinkElement to="#about" title="About" />
+                <LinkElement to="#services" title="Our Services" />
+                <LinkElement to="#contact" title="Contact" />
+                <LinkElement to='#login' title='Login'>
+                    <FaUserAlt />
+                </LinkElement>
+            </ul>
+        </div>
+    </nav>
+}
