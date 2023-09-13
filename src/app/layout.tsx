e2db127dd4250b9c9e7bd402ca42df6db22a8999
alok8bb <alok8bb@gmail.com>
2023-09-13 13:13:19 +0530

    fix footer
    Signed-off-by: alok8bb <alok8bb@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             >
                 <Navbar />
                 {children}
+                <Footer />
             </body>
         </html>
     );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+    return (
+        <footer className="flex flex-row w-full bg-white h-12 justify-end items-center absolute bottom-0 left-0 right-0">
+            <div className="px-6 font-bold flex gap-3 tracking-wider">
+                <a href="">Support</a>
+                <p>|</p>
+                <a href="">Privacy & Terms</a>
+                <p>|</p>
+                <a href="">Legal</a>
+            </div>
+        </footer>
+    );
+};
